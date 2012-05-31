f696ffc2a1c924d8c773ec411f0e1e782d83e77a
Johan Lindstrom <johan.lindstrom@net-a-porter.com>
2012-05-31 22:27:28 +0100

    symbol_server: "working" embryo of completion

--- source/lib/Devel/PerlySense/external/emacs/perly-sense-autocomplete.el
+++ source/lib/Devel/PerlySense/external/emacs/perly-sense-autocomplete.el
@@ -1,37 +1,43 @@
 
 
 (defun ps/ac-candidates ()
-  (let* (
-         (buffer-text (buffer-string))
-         (buffer-point (point))
-         (sub-name
-          ;; (with-temp-buffer
-          ;;            (insert buffer-text)
-          ;;            (goto-char buffer-point)
-          ;;            (sub-name (if (search-backward "sub +\\([_a-z0-9]+\\)" nil t)
-          ;;                          (match-string 1)
-          ;;                        "N/A")
-          ;;                      )
-          ;;            )
-          (format "buffer(%s) point(%s) prefix(%s)" ac-buffer ac-point ac-prefix)
-          )
-         )
-    (mapcar (lambda (candidate)
-              (format "%s-%s" candidate sub-name))
-            '("Foo" "Bar" "Baz" "Boo" "Boolean" "Bookshelf")
+  (save-excursion
+    (let* (
+           (sub-name
+            (if (search-backward-regexp "sub \\([a-zA-Z_]+\\)" nil t)
+                (match-string-no-properties 1)
+              "N/A")
             )
+           ;; (format "buffer(%s) point(%s) prefix(%s)" ac-buffer ac-point ac-prefix)
+           )
+      (mapcar (lambda (candidate)
+                (format "%s-%s" candidate sub-name))
+              '("Foo" "Bar" "Baz" "Boo" "Boolean" "Bookshelf")
+              )
+      )
     )
   )
 
-(defvar ps/ac-source
+(defun ps/candidate-documentation (symbol-name)
+  (let ((symbol-name (substring-no-properties symbol-name)))
+    "textClassInheritance(oClass)
+
+Return string representing the class hierarchy of $oClass."))
+
+(defvar ps/ac-source-method-calls
   '(
+    (prefix . "->\\(.*\\)")
     (candidates . ps/ac-candidates)
     (summary . (lambda () "Help text"))
+    (document . ps/candidate-documentation)
     )
   )
 
 
+; prefix: use 
 
-(setq ac-sources '(ps/ac-source))
+
+
+(setq ac-sources '(ps/ac-source-method-calls))
 
 
