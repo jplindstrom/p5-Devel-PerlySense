c56a5f129178647ab808e54e1b13c296b88eef7d
Johan Lindstrom <johan.lindstrom@net-a-porter.com>
2012-05-31 19:09:58 +0100

    symbol_server: More prototyping

--- source/lib/Devel/PerlySense/external/emacs/perly-sense-autocomplete.el
+++ source/lib/Devel/PerlySense/external/emacs/perly-sense-autocomplete.el
@@ -1,14 +1,21 @@
 
 
 (defun ps/ac-candidates ()
-  (let (
-        (sub-name (save-excursion
-                    (if (search-backward "sub +\\([_a-z0-9]+\\)" nil t)
-                        (match-string 1)
-                      "N/A")
-                    )
-                  )
-        )
+  (let* (
+         (buffer-text (buffer-string))
+         (buffer-point (point))
+         (sub-name
+          ;; (with-temp-buffer
+          ;;            (insert buffer-text)
+          ;;            (goto-char buffer-point)
+          ;;            (sub-name (if (search-backward "sub +\\([_a-z0-9]+\\)" nil t)
+          ;;                          (match-string 1)
+          ;;                        "N/A")
+          ;;                      )
+          ;;            )
+          (format "buffer(%s) point(%s) prefix(%s)" ac-buffer ac-point ac-prefix)
+          )
+         )
     (mapcar (lambda (candidate)
               (format "%s-%s" candidate sub-name))
             '("Foo" "Bar" "Baz" "Boo" "Boolean" "Bookshelf")
@@ -19,6 +26,7 @@
 (defvar ps/ac-source
   '(
     (candidates . ps/ac-candidates)
+    (summary . (lambda () "Help text"))
     )
   )
 
