658bde22a4901b8e52f616cc9ea242d53a797589
Johan Lindstrom <johan.lindstrom@net-a-porter.com>
2012-06-06 19:54:01 +0100

    symbol_server: Hard coded sqlite db

--- source/lib/Devel/PerlySense/Repository.pm
+++ source/lib/Devel/PerlySense/Repository.pm
@@ -5,16 +5,12 @@ package Devel::PerlySense::Repository;
 use Moose;
 use Method::Signatures;
 
-has oPerlySense => (is => "ro", isa => "Devel::PerlySense");
-
-sub BUILD {
-    my $self = shift;
-    my $file  = $self->oPerlySense->oHome->dirHomeRepository . "/proto.db";
-    warn "JPL: file is in (" . $file . ")\n";
-    my $create = q{
-    sub {
+use ORLite( {
+    package => "Devel::PerlySense::Repository::DB",
+    file    => "$ENV{HOME}/.PerlySense/repository/proto.db",
+    create  => sub {
         my $dbh = shift;
-        $dbh->do('
+            $dbh->do('
         CREATE TABLE method (
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL,
@@ -22,34 +18,39 @@ sub BUILD {
             documentation TEXT NOT NULL,
             file TEXT NOT NULL
         )');
-    }
-};
+    },
+    tables  => [ "method" ],
+} );
+
+
+has oPerlySense => (is => "ro", isa => "Devel::PerlySense");
+
+sub BUILD {
+    my $self = shift;
+    my $file  = $self->oPerlySense->oHome->dirHomeRepository . "/proto.db";
+    warn "JPL: file is in (" . $file . ")\n";
+    # my $create = q{
+    #};
     ## no critic
-    eval qq{
-        use ORLite {
-            package => "Devel::PerlySense::Repository::DB",
-            file    => '$file',
-            create  => $create,
-            tables  => [ "method" ],
-        };
-        1;
-    } or die;
+    # eval qq{
+    #     1;
+    # } or die;
 }
 
 method index( :$oDocument ) {
     my @aNamePackage = $oDocument->aNamePackage;
     $oDocument->determineLikelyApi(nameModule => $aNamePackage[0]); ###JPL: loop
-    
+
     Devel::PerlySense::Repository::DB->begin();
     Devel::PerlySense::Repository::DB::Method->delete(
-        "file = ?",
+        "where file = ?",
         $oDocument->file,
     );
     for my $package (keys %{$oDocument->rhPackageApiLikely}) {
         my $oApi = $oDocument->rhPackageApiLikely->{$package};
-        
+
     }
-    
+
     Devel::PerlySense::Repository::DB->commit();
 }
 
