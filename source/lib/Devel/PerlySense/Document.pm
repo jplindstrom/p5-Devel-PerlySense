19f631e868a4c956970e12d60bee106aed78ecdb
Johan Lindstrom <johan.lindstrom@broadbean.com>
2019-08-30 07:57:12 +0000

    RT#130415: escape { in regex

--- source/lib/Devel/PerlySense/Document.pm
+++ source/lib/Devel/PerlySense/Document.pm
@@ -1009,7 +1009,10 @@ my $matchReplace = {
     q/sub\s+\w+\s*:\s*\w+[^{]+{/ => q/SPECIAL/,
     q/^=(?:head|item|for|pod)/ => q/SPECIAL/,
 };
-my $rexMatch = join("|", keys %$matchReplace );
+my $rexMatch = join(
+    "|",
+    map { s/\{/\\{/; $_; } keys %$matchReplace,
+);
 sub _stringReplace {
     my ($match) = @_;
 
